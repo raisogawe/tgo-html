3064804e160612be19a5f91b716aa63d671e50e4
raisogawe <agusjusana@gmail.com>
2018-08-29 12:04:30 +0700

    Add files via upload

---
+++ calladsbyid.js
@@ -0,0 +1,46 @@
+<script>
+//<![CDATA[
+var element = document.getElementById("adslink1");
+element.innerHTML = "<ins class='adsbygoogle' data-ad-client='ca-pub-4411773773953927' data-ad-format='link' data-ad-slot='6253461479' data-full-width-responsive='true' style='display:block'></ins>";
+var newScript = document.createElement("script");
+var inlineScript = document.createTextNode("(adsbygoogle = window.adsbygoogle || []).push({});");
+newScript.appendChild(inlineScript);
+element.appendChild(newScript);;
+var element2 = document.getElementById("adsart2");
+element2.innerHTML = "<ins class='adsbygoogle' data-ad-client='ca-pub-4411773773953927' data-ad-layout='in-article' data-ad-format='fluid' data-ad-slot='2603595733' style='display:block; text-align:center;'></ins>";
+var newScript2 = document.createElement("script");
+var inlineScript2 = document.createTextNode("(adsbygoogle = window.adsbygoogle || []).push({});");
+newScript2.appendChild(inlineScript2);
+element2.appendChild(newScript2);;
+var element3 = document.getElementById("adsimgrspv2");
+element3.innerHTML = "<ins class='adsbygoogle' data-ad-client='ca-pub-4411773773953927' data-ad-format='auto' data-ad-slot='1232402801' style='display:block'></ins>";
+var newScript3 = document.createElement("script");
+var inlineScript3 = document.createTextNode("(adsbygoogle = window.adsbygoogle || []).push({});");
+newScript3.appendChild(inlineScript3);
+element3.appendChild(newScript3);;
+var element4 = document.getElementById("adsart1");
+element4.innerHTML = "<ins class='adsbygoogle' data-ad-client='ca-pub-4411773773953927' data-ad-layout='in-article' data-ad-format='fluid' data-ad-slot='1740563488' style='display:block; text-align:center;'></ins>";
+var newScript4 = document.createElement("script");
+var inlineScript4 = document.createTextNode("(adsbygoogle = window.adsbygoogle || []).push({});");
+newScript4.appendChild(inlineScript4);
+element4.appendChild(newScript4);;
+var element5 = document.getElementById("adsimgrspv1");
+element5.innerHTML = "<ins class='adsbygoogle' data-ad-client='ca-pub-4411773773953927' data-ad-format='auto' data-ad-slot='3794717464' style='display:block'></ins>";
+var newScript5 = document.createElement("script");
+var inlineScript5 = document.createTextNode("(adsbygoogle = window.adsbygoogle || []).push({});");
+newScript5.appendChild(inlineScript5);
+element5.appendChild(newScript5);;
+var element6 = document.getElementById("adsimgrspv3");
+element6.innerHTML = "<ins class='adsbygoogle' data-ad-client='ca-pub-4411773773953927' data-ad-format='auto' data-ad-slot='1172397902' style='display:block'></ins>";
+var newScript6 = document.createElement("script");
+var inlineScript6 = document.createTextNode("(adsbygoogle = window.adsbygoogle || []).push({});");
+newScript6.appendChild(inlineScript6);
+element6.appendChild(newScript6);;
+var element7 = document.getElementById("adsrespon");
+element7.innerHTML = "<ins class='adsbygoogle' data-ad-client='ca-pub-4411773773953927' data-ad-format='auto' data-ad-slot='8534243520' style='display:block'></ins>";
+var newScript7 = document.createElement("script");
+var inlineScript7 = document.createTextNode("(adsbygoogle = window.adsbygoogle || []).push({});");
+newScript7.appendChild(inlineScript7);
+element7.appendChild(newScript7);
+//]]>
+</script>
